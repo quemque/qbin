ab551cd3223a6c87cd6d56de002668cb234350cf
que <kosstetz@gmail.com>
2025-12-04 22:50:02 +0300

    first commit init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Биржа курсов валют</h1>
+      <p>Здесь будет отображаться курс валют</p>
+    </div>
+  );
+}
+
+export default App;
